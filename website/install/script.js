1a79e5fb4113ac77ff59670b09790f8ce19b1e41
Bug1312 <bug1312.babycarrots1312@gmail.com>
2022-10-29 23:23:54 -0400

    fixes

--- website/install/script.js
+++ website/install/script.js
@@ -18,13 +18,13 @@ function addVersion(version, date, changelogURL) {
     <div class="version">
         <span class="version-version">${version}</span>
         <span class="version-time">${date}</span>
-        <a class="version-file version-underline" href="https://github.com/${repo}/releases/download/${version}/java_json.js" download="java_json.js"> <i class="bi bi-file-earmark-arrow-down"></i> File </a>
+        <a class="version-file version-underline" href="https://github.com/${repo}/releases/download/${version}/dm_java_json.js" download="dm_java_json.js"> <i class="bi bi-file-earmark-arrow-down"></i> File </a>
         <div class="tooltip" style="width:initial;">
             <span class="tooltiptext version-copy-tooltip">
                 Copy to clipboard
             </span>
             <a class="version-url version-underline" onclick="copy('version_${version}-url-copy')" onmouseleave="resetCopyTooltip()"> <i class="bi bi-link-45deg"></i> URL </a>
-            <input id="version_${version}-url-copy" type="text" value="https://github.com/${repo}/releases/download/${version}/java_json.js" readonly hidden>
+            <input id="version_${version}-url-copy" type="text" value="https://github.com/${repo}/releases/download/${version}/dm_java_json.js" readonly hidden>
         </div>
         <a class="version-changelog version-underline" href="${changelogURL}"> <i class="bi bi-file-earmark-text"></i> Changelog </a>
     </div>`;
